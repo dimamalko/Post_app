d5515fe7c3821b01e64d06dd62eddc0178555487
Dima Malko <dmytro.malko11@gmail.com>
2020-04-13 14:50:33 +0200

    New header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,15 +1,26 @@
 import './Header.scss';
+import Logo from '../../Images/logo.png';
 import React from 'react';
 import i18n from '../../services/i18n';
 
 export default function Header() {
   return (
     <div className="header">
-      <nav className="header__navigation">
-        <a href="/">{i18n.t('general.home')}</a>
-        <a href="/About">{i18n.t('general.about')}</a>
-        <a href="/Contact">{i18n.t('general.contact')}</a>
+      <nav className="header__bar">
+        <img className="header__bar--img" src={Logo} />
+        <div className="header__navigation">
+          <a className="header__bar--destination" href="/">
+            {i18n.t('general.home')}
+          </a>
+          <a className="header__bar--destination" href="/About">
+            {i18n.t('general.about')}
+          </a>
+          <a className="header__bar--destination" href="/Contact">
+            {i18n.t('general.contact')}
+          </a>
+        </div>
       </nav>
+      <img className="header__logo" src={Logo} />
     </div>
   );
 }
